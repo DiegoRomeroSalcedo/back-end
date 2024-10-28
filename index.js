1074f33503e2b9b102e7e1327e8f1a54a8b6f6f6
DiegoRomeroSalcedo <diegosalcedoromero835@gmail.com>
2024-10-28 16:27:33 -0500

    Se ajusta el procces.env.PORT

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.delete('/api/notes/:id', (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server runnig in port ${PORT}`);
 });
